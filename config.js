8e9923217d5d540e6e0cc09ed02ccbd6b879c96b
AgusNihTamplengDong <jiwasanjaya81@gmail.com>
2025-02-07 08:58:36 +0700

    ShoyuCrasher

---
+++ config.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [5967379234], // ganti jadi id mu
+    botToken: "7607070678:AAFm24hJ9eOCU34gNYcBQgm91VwREd6SpC4", //isi make token bot mu
+}
+
+module.exports = global;
